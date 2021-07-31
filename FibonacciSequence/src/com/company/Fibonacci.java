4ebcb1e8ee480b6a18dbb0d8cb76ccf413ef6a41
Maks <off_white_off@mail.ru>
2021-07-31 20:45:36 +0300

    Memoization is added in the Fibonacci Sequence

--- FibonacciSequence/src/com/company/Fibonacci.java
+++ FibonacciSequence/src/com/company/Fibonacci.java
@@ -1,9 +1,17 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(getFibNumFast(100));
+        int n = 7;
+        long[] memoization = new long[n + 1];
+        Arrays.fill(memoization, -1);
+
+        System.out.println(getFibNumFast2(n, memoization));
+        System.out.println(getFibNumFast(7));
+
     }
 
     // быстрый способ
@@ -19,17 +27,30 @@ public class Fibonacci {
             for (int i = 2; i <= n; i++) {
                 arr[i] = arr[i - 1] + arr[i - 2];
             }
-
             return arr[n];
         }
     }
 
-    // медленный способ из-за лишних вычислений
+    // рекурсией, но исключая повторные вычисления
+    private static long getFibNumFast2(int n, long[] memoization){
+        if(n <= 1)
+            return n;
+
+        if(memoization[n] != -1)
+            return memoization[n];
+        
+        long result = getFibNumFast2(n-1, memoization) + getFibNumFast2(n-2, memoization);
+        memoization[n] = result;
+
+        return memoization[n];
+    }
+
+    // медленный способ из-за лишних вычислений в рекурсии
     private static long getFibNum(int n){
         if(n <= 1){
             return n;
         }
-        
+
         return getFibNum(n-1) + getFibNum(n-2);
     }
 }
