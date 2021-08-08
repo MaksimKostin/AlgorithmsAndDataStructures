ed1d4a16c1c18bae03f6a6dcd2ed7a5fcddcb55d
Maks <off_white_off@mail.ru>
2021-08-08 19:31:12 +0300

    Added an improved version of BubbleSort

--- BubbleSort/src/com/company/BubbleSort.java
+++ BubbleSort/src/com/company/BubbleSort.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {3, 7, 0, 5, 9, 4, 1, 8, 9};
-        sortedByBubbleSort(arr);
+        int[] arr = {3, 7, 0, 5, 9, 4, 1, 8, 9, -1, -1};
+        sortedByBubbleSortV2(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     // O(n^2)
-    private static void sortedByBubbleSort(int[] arr){
+    private static void sortedByBubbleSortV1(int[] arr){
         boolean isSorted = false;
         while(!isSorted){
             isSorted = true;
@@ -23,6 +23,21 @@ public class BubbleSort {
                     isSorted = false;
                 }
             }
+        }
+    }
+
+    // улучшенная версия тк нет лишних сравнений
+    private static void sortedByBubbleSortV2(int[] arr){
+        int count = arr.length - 1;
+        while(count > 0){
+            for (int i = 0; i < count; i++) {
+                if(arr[i] > arr[i+1]){
+                    int temp = arr[i+1];
+                    arr[i+1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            count--;
         }
     }
 }
