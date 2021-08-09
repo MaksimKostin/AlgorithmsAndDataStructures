a4ea9fed4044a4207dddb9bcaacd5f1ebc83a6ab
Maks <off_white_off@mail.ru>
2021-08-09 22:09:29 +0300

    Added some comments to QuickSort

--- QuickSort/src/com/company/Main.java
+++ QuickSort/src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
     }
 
     private static void quickSort(int[] arr, int from, int to){
-        if(from < to){
+        if(from < to){ // проверка на размерность массива(может состоять из 1 эл-та поэтому не нужно сортировать)
             int divideIndex = partition(arr, from, to);
             quickSort(arr, from, divideIndex - 1);
             quickSort(arr, divideIndex, to);
@@ -22,14 +22,14 @@ public class Main {
         int leftIndex= from;
         int rightIndex= to;
         int pivot = arr[(from+to)/2];
-        while(leftIndex <= rightIndex){
+        while(leftIndex <= rightIndex){ // проверка на полное прохождение и сортировку подмассива
             while (arr[leftIndex] < pivot){
                 leftIndex++;
             }
             while(arr[rightIndex] > pivot){
                 rightIndex--;
             }
-            if(leftIndex <= rightIndex){
+            if(leftIndex <= rightIndex){ // предотвращение swap для отсортированных пар
                 swap(arr, leftIndex, rightIndex);
                 leftIndex++;
                 rightIndex--;
