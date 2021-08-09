cd90b3278b84cbcb926ffe9c109204db1bc85f67
Maks <off_white_off@mail.ru>
2021-08-09 19:42:22 +0300

    QuickSort done

---
+++ QuickSort/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] arr = {4, 7, 9, 4 , 6, 5, 4, 9, 0};
+	    quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int from, int to){
+        if(from < to){
+            int divideIndex = partition(arr, from, to);
+            quickSort(arr, from, divideIndex - 1);
+            quickSort(arr, divideIndex, to);
+        }
+    }
+
+    private static int partition(int[] arr, int from, int to){
+        int leftIndex= from;
+        int rightIndex= to;
+        int pivot = arr[(from+to)/2];
+        while(leftIndex <= rightIndex){
+            while (arr[leftIndex] < pivot){
+                leftIndex++;
+            }
+            while(arr[rightIndex] > pivot){
+                rightIndex--;
+            }
+            if(leftIndex <= rightIndex){
+                swap(arr, leftIndex, rightIndex);
+                leftIndex++;
+                rightIndex--;
+            }
+        }
+        return leftIndex;
+    }
+
+    private static void swap(int[] arr, int firstIndex, int secondIndex){
+        int temp = arr[firstIndex];
+        arr[firstIndex] = arr[secondIndex];
+        arr[secondIndex] = temp;
+    }
+}
